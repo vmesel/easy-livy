eff5bdbc7f8a02f31c7563ff3760c516c4091164
Vinicius Mesel <bioinfo.vinicius@gmail.com>
2020-08-07 11:52:30 -0300

    Create easylivy.py

---
+++ easylivy.py
@@ -0,0 +1,31 @@
+class LivyConnector:
+    def __init__(self, livy_url):
+        self.livy_url = livy_url
+    
+    def create_session(self, session_name="mr-livy"):
+        directive = '/sessions'
+        headers = {'Content-Type': 'application/json'}
+
+        data = {
+            'kind': 'pyspark',
+            'name': '{}'.format(session_name)
+        }
+
+        resp = requests.post(self.livy_url+directive, headers=headers, data=json.dumps(data))
+
+        if resp.status_code == requests.codes.created:
+            self.session_id = resp.json()['id']
+        else:
+            ValueError("Session with same name already created")
+
+    def run_statement(self, statement):
+        directive = f'/sessions/{self.session_id}/statements'
+        resp = requests.post(
+            self.livy_url + directive,
+            headers=headers,
+            data=json.dumps(statement)
+        )
+        return resp.json()
+        
+    def retrieve_statement_result(self, statement_id):
+        return requests.get(self.livy_url + f'/sessions/{self.session_id}/statements/{statement_id}').json()
